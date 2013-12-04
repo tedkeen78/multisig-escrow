46fd2476aaa75a6183387f0a7e1097400978910f
Chris Cowan <agentme49@gmail.com>
2013-12-03 21:21:08 -0600

    Add db indexes. Make arbitrator_address and _privkey fields unique.

--- init-sqlite.sql
+++ init-sqlite.sql
@@ -1,28 +1,27 @@
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` INTEGER NOT NULL,
+  `id` INTEGER PRIMARY KEY UNIQUE NOT NULL,
   `email` TEXT UNIQUE NOT NULL,
   `otpenabled` BOOLEAN DEFAULT 0 NOT NULL,
   `otpkey` TEXT,
-  `emailoptout` BOOLEAN DEFAULT 0 NOT NULL,
-  PRIMARY KEY (`id` ASC)
+  `emailoptout` BOOLEAN DEFAULT 0 NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `transactions` (
-  `id` INTEGER NOT NULL,
+  `id` INTEGER PRIMARY KEY UNIQUE NOT NULL,
   `uuid` TEXT UNIQUE NOT NULL,
-  `buyer` INTEGER,
-  `seller` INTEGER,
-  `arbitrator` INTEGER,
+  `buyer` INTEGER REFERENCES `users`(`id`) ON DELETE SET NULL,
+  `seller` INTEGER REFERENCES `users`(`id`) ON DELETE SET NULL,
+  `arbitrator` INTEGER REFERENCES `users`(`id`) ON DELETE SET NULL,
   `buyer_agreed` BOOLEAN DEFAULT 0 NOT NULL,
   `seller_agreed` BOOLEAN DEFAULT 0 NOT NULL,
   `arbitrator_agreed` BOOLEAN DEFAULT 0 NOT NULL,
   `inv_secret` TEXT NOT NULL,
   `buyer_address` TEXT,
   `seller_address` TEXT,
-  `arbitrator_address` TEXT,
-  `arbitrator_privkey` TEXT,
+  `arbitrator_address` TEXT UNIQUE,
+  `arbitrator_privkey` TEXT UNIQUE,
   `title` TEXT NOT NULL,
   `text` TEXT NOT NULL,
   `time_made` INTEGER NOT NULL,
@@ -32,22 +31,28 @@ CREATE TABLE IF NOT EXISTS `transactions` (
   `time_complete` INTEGER,
   `timelength` INTEGER,
   `payment` INTEGER NOT NULL,
-  `arb_fee` REAL NOT NULL,
-  PRIMARY KEY (`id` ASC),
-  FOREIGN KEY (`buyer`) REFERENCES `users`(`id`) ON DELETE SET NULL,
-  FOREIGN KEY (`seller`) REFERENCES `users`(`id`) ON DELETE SET NULL,
-  FOREIGN KEY (`arbitrator`) REFERENCES `users`(`id`) ON DELETE SET NULL
+  `arb_fee` REAL NOT NULL
 );
 
+CREATE INDEX IF NOT EXISTS `transactions_buyer_idx` ON `transactions`(`buyer`);
+CREATE INDEX IF NOT EXISTS `transactions_seller_idx` ON `transactions`(`seller`);
+CREATE INDEX IF NOT EXISTS `transactions_arbitrator_idx` ON `transactions`(`arbitrator`);
+CREATE INDEX IF NOT EXISTS `transactions_time_made_idx` ON `transactions`(`time_made`);
+CREATE INDEX IF NOT EXISTS `transactions_time_canceled_idx` ON `transactions`(`time_canceled`);
+CREATE INDEX IF NOT EXISTS `transactions_time_started_idx` ON `transactions`(`time_started`);
+CREATE INDEX IF NOT EXISTS `transactions_time_fundsactive_idx` ON `transactions`(`time_fundsactive`);
+CREATE INDEX IF NOT EXISTS `transactions_time_complete_idx` ON `transactions`(`time_complete`);
+-- SQLite creates implicit indexes on unique and primary key fields, so
+-- we don't need to worry about creating indexes for those fields.
+
 CREATE TABLE IF NOT EXISTS `transactioncomments` (
-  `id` INTEGER NOT NULL,
-  `transaction` INTEGER NOT NULL,
+  `id` INTEGER PRIMARY KEY UNIQUE NOT NULL,
+  `transaction` INTEGER NOT NULL REFERENCES `transactions`(`id`) ON DELETE CASCADE,
   `commentnum` INTEGER NOT NULL,
-  `user` INTEGER,
+  `user` INTEGER REFERENCES `users`(`id`) ON DELETE SET NULL,
   `time` INTEGER NOT NULL,
   `text` TEXT NOT NULL,
-  PRIMARY KEY (`id` ASC),
-  UNIQUE (`transaction`, `commentnum`),
-  FOREIGN KEY (`user`) REFERENCES `users`(`id`) ON DELETE SET NULL,
-  FOREIGN KEY (`transaction`) REFERENCES `transactions`(`id`) ON DELETE CASCADE
+  UNIQUE (`transaction`, `commentnum`)
 );
+
+CREATE INDEX IF NOT EXISTS `transactioncomments_transaction_idx` ON `transactioncomments`(`transaction`);
